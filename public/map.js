bd3290ec57094e39ddf947c101d82d08ebaf90f3
srthurman <srthurman@gmail.com>
2015-08-30 18:34:37 +0000

    Only show metro stations in buffer

--- public/map.js
+++ public/map.js
@@ -1,19 +1,21 @@
 var buffer = require('turf-buffer');
 var within = require('turf-within');
 var fc = require('turf-featurecollection');
+var point = require('turf-point');
 
 var $ = require('jquery');
 //var turf = require('turf');
 
 var baseURI = window.location.href;
+var currentRadius = 0.5;
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoic3J0aHVybWFuIiwiYSI6IkVGXy1NMHcifQ.EouINDEZGzjGs0x0VMhHxg';
 
 var map = L.mapbox.map('map','srthurman.n9l71i86')
-    .setView([38.901078, -77.024361], 14);
+    .setView([38.901078, -77.024361], 15);
     
 var wmataRoutes = L.mapbox.featureLayer()
-    .loadURL('https://transitnearme-srthurman.c9.io/wmataRoutes')
+    .loadURL(baseURI+'wmataRoutes')
     .addTo(map);
     
 var marker = L.marker(new L.LatLng(38.901078, -77.024361), {
@@ -27,34 +29,56 @@ var marker = L.marker(new L.LatLng(38.901078, -77.024361), {
     zIndexOffset:999
 }).addTo(map);
 
-var bufferLayer = L.mapbox.featureLayer().addTo(map);
 
-var bufferCntr = marker.getLatLng();
-var pt = {
-  "type": "Feature",
-  "properties": {},
-  "geometry": {
-    "type": "Point",
-    "coordinates": [bufferCntr.lng, bufferCntr.lat]
-  }
-};
+$.get(baseURI+'wmataStops', function(data) {
+    //click-move functionality
+    map.on('click',function(e){
+        marker.setLatLng([e.latlng.lat, e.latlng.lng]);
+        map.setView([e.latlng.lat, e.latlng.lng],15);
+        updateTransitPoints();
+    });
+    
+    // get position, get radius, draw buffer, find within, calculate distances, find nearest, add to map
+    function updateTransitPoints(){
+        $('path').remove();
+        $('.leaflet-marker-pane *').not(':first').remove();
+        var position=marker.getLatLng();
+        var pt=point([position.lng, position.lat]);
 
-var transitBuffer = buffer(pt, 0.5, 'miles');
-
-transitBuffer.properties = {
-                "fill": "#00704A",
-                "fill-opacity":0.05,
-                "stroke": "#00704A",
-                "stroke-width": 2,
-                "stroke-opacity": 0.5
+        //draw buffer
+        var bufferLayer = L.mapbox.featureLayer().addTo(map);
+        
+        var transitBuffer = buffer(pt, currentRadius, 'miles');
+        transitBuffer.properties = {
+            "fill": "#00704A",
+            "fill-opacity":0.05,
+            "stroke": "#00704A",
+            "stroke-width": 2,
+            "stroke-opacity": 0.5
+        };
+        
+        bufferLayer.setGeoJSON(transitBuffer);
+        
+        //load stops and filter based on buffer
+        var filteredStops = L.mapbox.featureLayer().addTo(map);
+        var stops = within(data, fc([transitBuffer]));
+        console.log(stops);
+        
+        var metroStops = [];
+        for (s=0,l=stops.features.length;s<l;s++) {
+            var currStop = stops.features[s];
+            var str = currStop['name'];
+            var re = /metro station/i;
+            var found = str.match(re);
+            if (found) {
+                metroStops.push(currStop);
             };
-
-bufferLayer.setGeoJSON(transitBuffer);
-
-//load stops and filter based on buffer
-var filteredStops = L.mapbox.featureLayer().addTo(map);
-
-$.get('https://transitnearme-srthurman.c9.io/wmataStops', function(data) {
-    var stops = within(data, fc([transitBuffer]));
-    filteredStops.setGeoJSON(stops);
+        }
+        console.log(fc([metroStops]).features[0]);
+        filteredStops.setGeoJSON(fc([metroStops]).features[0]);
+        
+        $('#busStopTally').html(stops.features.length);
+    }
+    marker.on('drag', function(){updateTransitPoints()});
+    updateTransitPoints();
 });
